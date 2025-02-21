241b33adbfbe88bc4408347264d2c1c785fd6a45
Kajetan <135659720+Kajetaan@users.noreply.github.com>
2025-02-21 14:28:19 +0100

    solve second excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, nmb) {
+    let modified_str = "";
+    if (nmb < 0) {
+        modified_str = "ERROR";
+    } else {
+        for (let i = 0; i <= nmb - 1; i++) {
+            modified_str = modified_str + str;
+        }
+    }
 
+
+    return modified_str;
 };
 
 // Do not edit below this line
