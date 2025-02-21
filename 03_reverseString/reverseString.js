e27e5b03ffef91b20472332737b3bc8d06db7afc
Kajetan <135659720+Kajetaan@users.noreply.github.com>
2025-02-21 15:03:35 +0100

    solve excercise 3 without arrays

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(world) {
 
+    let world_reversed = "";
+    let position = world.length;
+    for(i = 0; i < position; i++){
+        world_reversed += world[position-1-i];    
+    }
+    return world_reversed;
 };
 
 // Do not edit below this line
