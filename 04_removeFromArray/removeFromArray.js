21946be0c21726795454e4fcd1b20cbd9bdcf3df
Kajetan <135659720+Kajetaan@users.noreply.github.com>
2025-02-22 19:28:55 +0100

    solve the exc 4, yes I copied the solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const { cobolCase } = require("case-anything");
 
+const removeFromArray = function(collection, ...elements) {
+    newCollection = []; 
+    collection.forEach((element) => {
+        if(!elements.includes(element)){
+            newCollection.push(element);
+        }
+    });
+
+    return newCollection;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
