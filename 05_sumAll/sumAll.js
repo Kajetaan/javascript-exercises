9d6d12174b400aeb29e9345999cb4e2d9da1261b
Kajetan <135659720+Kajetaan@users.noreply.github.com>
2025-03-12 19:46:48 +0100

    add a solution for the excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (numb, second_number) {
 
+
+    if (typeof numb !== "number" ||
+        typeof second_number !== "number" ||
+        !Number.isInteger(numb) ||
+        !Number.isInteger(second_number) ||
+        numb < 0 ||
+        second_number < 0) {
+        return "ERROR";
+    }
+
+    let start = Math.min(numb, second_number);
+    let finish = Math.max(numb, second_number);
+
+    let sum = 0;
+    while (start <= finish) {
+        sum += start;
+        start++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
